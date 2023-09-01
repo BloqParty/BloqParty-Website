ced8f24e1e29ad432372fd3334c2f50360e461d2
Syl <hi@sylviiu.dev>
2023-09-01 18:22:47 -0500

    create general server handler

---
+++ index.js
@@ -0,0 +1,69 @@
+require(`./core/logger`)();
+
+const fs = require('fs');
+
+const config = require(`./core/config`)
+
+const express = require('express');
+const next = require('next');
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    console.debug('Starting server...');
+
+    const server = express();
+
+    const endpoints = fs.readdirSync(`./src/endpoints`).map(s => {
+        const module = require(`./src/endpoints/${s}`);
+
+        return {
+            file: s,
+            name: s.split(`.`).slice(0, -1).join(`.`),
+            render: fs.existsSync(`./src/pages/${s.split(`.`).slice(0, -1).join(`.`)}.jsx`),
+            method: (module.method && typeof server[module.method.toLowerCase()] == `function`) ? module.method.toLowerCase() : `get`,
+            endpoints: typeof module.endpoint == `string` ? [module.endpoint] : module.endpoint,
+        };
+    });
+
+    console.debug(`Found ${endpoints.length} (${endpoints.filter(o => o.render).length} react) endpoints! [ ${endpoints.map(o => o.name).join(`, `)} ]`);
+
+    for(const endpoint of endpoints) {
+        if(endpoint.render) {
+            console.debug(`Setting up react endpoint: ${endpoint.name} [ ${endpoint.endpoints.join(`, `)} ]`);
+
+            for(const path of endpoint.endpoints) {
+                server[endpoint.method](path, (req, res) => {
+                    return app.render(req, res, `/${endpoint.name}`, req.query);
+                });
+
+                console.debug(`| Set up react endpoint: "${path}" -> ${endpoint.name}`);
+            }
+        } else {
+            console.debug(`Setting up regular endpoint: ${endpoint.name} [ ${endpoint.endpoints.join(`, `)} ]`);
+
+            for(const path of endpoint.endpoints) {
+                server[endpoint.method](path, (req, res) => {
+                    return require(`./src/endpoints/${endpoint.file}`)(req, res);
+                });
+
+                console.debug(`| Set up regular endpoint: "${path}" -> ${endpoint.name}`);
+            }
+        }
+    };
+
+    console.debug(`Setting up next handler...`);
+
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    console.debug(`Setting up server listener...`);
+
+    server.listen(config.port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on port ${config.port}`);
+    });
+}).catch(console.error)
