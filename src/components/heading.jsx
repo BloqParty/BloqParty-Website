e28ef64a12b1eee03a63b4d183496cef02cfa99a
Syl <hi@sylviiu.dev>
2023-09-02 22:36:13 -0500

    much more dynamic heading; support more details

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -1,20 +1,105 @@
 import React, { Component } from 'react';
+import DetailBlock from './leaderboard/detailblock';
+
+const commonStyle = {
+    boxSizing: `border-box`,
+    minWidth: `400px`,
+    width: `min(75vw, 1230px)`,
+    borderRadius: `10px`,
+}
 
 export default class Heading extends Component {
-    render() {
+    image() {
+        return (
+            <>
+                { this.props.image ? <img src={this.props.image} style={{
+                    width: `100px`,
+                    borderRadius: `10px`,
+                    marginRight: `20px`,
+                }} /> : null }
+            </>
+        )
+    }
+
+    text() {
         return (
             <div style={{
-                display: `flex`,
-                flexDirection: `column`,
-                alignItems: `center`,
-                justifyContent: `center`,
-                padding: `20px`,
-                boxSizing: `border-box`,
-                textAlign: `center`,
+                display: `block`
             }}>
-                { this.props.title ? <h1>{this.props.title}</h1> : null }
+                { this.props.title ? <h1 id="title">{this.props.title}</h1> : null }
+                { this.props.artist ? <h3 id="artist">{this.props.artist}</h3> : null }
+                { this.props.mapper ? <h4 id="mapper">Mapped by: {this.props.mapper}</h4> : null }
+                { this.props.description ? <h5 id="description">{this.props.description}</h5> : null }
+            </div>
+        )
+    }
 
-                { this.props.description ? <h3>{this.props.description}</h3> : null }
+    tags() {
+        return (
+            <>
+                { this.props.tags ? this.props.tags.map(({icon, value, title, color}) => (
+                    <DetailBlock icon={icon} value={value} title={title} color={color} />
+                )) : null }
+            </>
+        )
+    }
+
+    render() {
+        console.log(this.props);
+
+        const text = this.text();
+        const image = this.image();
+        const tags = this.tags();
+
+        return (
+            <div id="headingbg" style={{
+                marginBottom: `50px`,
+                ...commonStyle,
+                ...(this.props.image ? {
+                    backgroundImage: `url("${this.props.image}")`,
+                    backgroundSize: `cover`,
+                    backgroundPosition: `center`,
+                    backgroundRepeat: `no-repeat`,
+                } : {
+                    backgroundColor: `rgba(0, 0, 0, 0.4)`,
+                })
+            }}>
+                <div id="heading" style={{
+                    ...commonStyle,
+                    display: `flex`,
+                    flexDirection: `column`,
+                    alignItems: `start`,
+                    justifyContent: `center`,
+                    padding: `30px 20px`,
+                    textAlign: `left`,
+                    backdropFilter: this.props.image ? `blur(10px) grayscale(40%) brightness(0.6)` : null
+                }}>
+                    { image ? (
+                        <div style={{
+                            display: `flex`,
+                            flexDirection: `row`,
+                            alignItems: `center`,
+                            justifyContent: `center`,
+                        }}>
+                            {image}
+                            {text}
+                        </div>
+                    ) : (
+                        {text}
+                    )}
+
+                    { tags ? (
+                        <div style={{
+                            display: `flex`,
+                            flexDirection: `row`,
+                            alignItems: `center`,
+                            justifyContent: `center`,
+                            marginTop: `20px`,
+                        }}>
+                            {tags}
+                        </div>
+                    ) : null }
+                </div>
             </div>
         );
     }
