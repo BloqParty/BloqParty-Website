21aa78659416293d5bcc791bb6787497f70eea03
Syl <hi@sylviiu.dev>
2023-09-02 22:37:45 -0500

    add image to bp main page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,6 +4,6 @@ import Heading from '../components/heading'
 
 export default function Landing() {
     return (
-        <Heading title="Bedroom Party Leaderboard" description="can i get a uhhhhhhhhhhhhhhhh" />
+        <Heading image="https://cdn.discordapp.com/attachments/1097202703026880652/1133771191950250035/02.png?size=512" title="Bedroom Party Leaderboard" description="can i get a uhhhhhhhhhhhhhhhh" />
     )
 }
