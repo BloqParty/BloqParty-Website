78688dab21a1ea52f199ca60fba2b847251d19f6
Syl <hi@sylviiu.dev>
2023-09-03 22:09:30 -0500

    separate leaderboard entries to its own component

--- src/components/leaderboard.jsx
+++ src/components/leaderboard.jsx
@@ -1,116 +1,38 @@
 import React, { Component } from 'react';
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 import Error from './errormsg'
-import DetailBlock from './leaderboard/detailblock';
 
-const row = {
-    display: `flex`,
-    flexDirection: `row`,
-    alignItems: `center`,
-    justifyContent: `center`,
-}
+import Entry from './leaderboard/entry';
 
 export default class Leaderboard extends Component {
     render() {
         const offset = this.props.offset;
 
+        const error = this.props.error;
+
         const entries = this.props.entries;
 
-        if(Array.isArray(entries)) {
+        if(Array.isArray(entries) && entries.length && (!error || !Object.keys(error).length)) {
             return (
                 <div style={{
                     display: `flex`,
                     flexDirection: `column`,
-                    alignItems: `center`,
+                    alignItems: `baseline`,
                     justifyContent: `center`,
                     marginBottom: `8px`,
-                    padding: `0px 20px`,
                     boxSizing: `border-box`,
                     textAlign: `center`,
+                    width: `75vw`,
+                    maxWidth: `750px`,
+                    padding: `25px`,
+                    overflowX: `scroll`,
                 }}>
                     {
-                        entries.map((entry, index) => {
-                            return (
-                                <div style={{
-                                    display: `flex`,
-                                    flexDirection: `row`,
-                                    alignItems: `center`,
-                                    justifyContent: `center`,
-                                    marginBottom: `8px`,
-                                    padding: `0px 20px`,
-                                    boxSizing: `border-box`,
-                                    textAlign: `center`,
-                                    minWidth: `700px`,
-                                    boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`,
-                                }}>
-                                    <div style={{
-                                        ...row,
-                                        justifyContent: `left`,
-                                    }}>
-                                        <p style={{
-                                            fontWeight: `bold`,
-                                            fontSize: `1.5em`,
-                                            marginRight: `20px`,
-                                        }}>{Number(index) + 1 + (offset || 0)}</p>
-
-                                        <img src={entry.image || require(`../../core/config`).defaultImage} style={{
-                                            backgroundSize: `cover`,
-                                            backgroundPosition: `center`,
-                                            backgroundRepeat: `no-repeat`,
-                                            backgroundColor: `rgba(0, 0, 0, 0.5)`,
-                                            boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`,
-                                            borderRadius: `100%`,
-                                            marginRight: `20px`,
-                                            width: `60px`,
-                                            height: `60px`,
-                                        }} />
-
-                                        {
-                                            entry.name ? (
-                                                <p style={{
-                                                    fontSize: `1.5em`,
-                                                }}>{entry.name}</p>
-                                            ) : entry.id ? (
-                                                <>
-                                                    <p style={{
-                                                        fontSize: `1.3em`,
-                                                        fontStyle: `italic`,
-                                                        opacity: 0.7,
-                                                    }}>{entry.id}</p>
-                                                    <DetailBlock width="40px" color="#333" icon={icon({name: 'circle-info'})} title="There is no username on this account; the score ID is shown instead." value="ID" />
-                                                </>
-                                            ) : (
-                                                <>
-                                                    <p style={{
-                                                        fontSize: `1.1em`,
-                                                        fontStyle: `italic`,
-                                                        opacity: 0.4,
-                                                    }}>{`--`}</p>
-                                                    <DetailBlock width="28px" color="#333" icon={icon({name: 'circle-info'})} title="There is no username or score ID to show." value="ID" />
-                                                </>
-                                            )
-                                        }
-                    
-                                        
-                                    </div>
-                    
-                                    <div style={{
-                                        ...row,
-                                        justifyContent: `right`,
-                                        flexGrow: 1,
-                                    }}>
-                                        <h4>{entry.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h4>
-                                        <DetailBlock color="#db515f" icon={icon({name: 'x'})} title={`${entry.misses || 0} miss / ${entry.badCuts || 0} bad cut`} value={((entry.misses || 0) + (entry.badCuts || 0)) || 0} />
-                                        { entry.fullCombo ? <DetailBlock color="linear-gradient(135deg, rgb(112,143,255) 0%, rgb(198,128,237) 100%)" title="Full Combo" value="FC" /> : null }
-                                    </div>
-                                </div>
-                            )
-                        })
+                        entries.map((entry, index) => (<Entry position={Number(index) + 1 + (offset || 0)} entry={entry} key={entry.key || entry.value} />))
                     }
                 </div>
             );
-        } else return (
-            <Error title="No entries" description="There are no entries in this leaderboard." />
+        } else if(error) return (
+            <Error title={error?.title || (!entries.length ? `No entries` : `Error`)} description={error?.description || (!entries.length ? `There are no entries in this leaderboard` : `Unknown error.`)} />
         )
     }
 }
--- src/components/leaderboard/entry.jsx
+++ src/components/leaderboard/entry.jsx
@@ -1,21 +1,105 @@
 import React, { Component } from 'react';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import DetailBlock from '../detailblock';
 
-export default class Leaderboard extends Component {
+const row = {
+    display: `flex`,
+    flexDirection: `row`,
+    alignItems: `center`,
+    justifyContent: `center`,
+}
+
+export default class Entry extends Component {
     render() {
+        const { entry, position } = this.props;
+
         return (
             <div style={{
                 display: `flex`,
-                flexDirection: `column`,
+                flexDirection: `row`,
                 alignItems: `center`,
                 justifyContent: `center`,
-                padding: `20px`,
+                marginBottom: `8px`,
+                padding: `0px 20px`,
                 boxSizing: `border-box`,
                 textAlign: `center`,
+                minWidth: `600px`,
+                width: `100%`,
+                height: `60px`,
+                borderRadius: `10px`,
+                backgroundColor: `rgba(0, 0, 0, 0.2)`,
+                boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`
             }}>
-                { this.props.title ? <h1>{this.props.title}</h1> : null }
+                <div style={{
+                    ...row,
+                    justifyContent: `left`,
+                }}>
+                    <p style={{
+                        fontWeight: `bold`,
+                        fontSize: `1.5em`,
+                        marginRight: `20px`,
+                    }}>{position}</p>
 
-                { this.props.description ? <h3>{this.props.description}</h3> : null }
+                    <img src={entry.image} style={{
+                        backgroundSize: `cover`,
+                        backgroundPosition: `center`,
+                        backgroundRepeat: `no-repeat`,
+                        backgroundColor: `rgba(0, 0, 0, 0.5)`,
+                        boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`,
+                        borderRadius: `100%`,
+                        marginRight: `20px`,
+                        width: `47px`,
+                        height: `47px`,
+                    }} />
+
+                    {
+                        entry.username ? (
+                            <p style={{
+                                fontSize: `1.5em`,
+                            }}>{entry.username}</p>
+                        ) : entry.id ? (
+                            <>
+                                <p style={{
+                                    fontSize: `1.3em`,
+                                    fontStyle: `italic`,
+                                    opacity: 0.7,
+                                }}>{entry.id}</p>
+                                <DetailBlock width="40px" color="#333" icon={icon({name: 'circle-info'})} title="There is no username on this account; the score ID is shown instead." value="ID" />
+                            </>
+                        ) : (
+                            <>
+                                <p style={{
+                                    fontSize: `1.1em`,
+                                    fontStyle: `italic`,
+                                    opacity: 0.4,
+                                }}>{`--`}</p>
+                                <DetailBlock width="28px" color="#333" icon={icon({name: 'circle-info'})} title="There is no username or score ID to show." value="ID" />
+                            </>
+                        )
+                    }
+
+                    
+                </div>
+
+                <div style={{
+                    ...row,
+                    justifyContent: `right`,
+                    flexGrow: 1,
+                }}>
+                    <h4>{entry.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h4>
+
+                    {
+                        entry.fullCombo ? (
+                            <DetailBlock color="linear-gradient(135deg, rgb(112,143,255) 0%, rgb(198,128,237) 100%)" title="Full Combo" value="FC" />
+                        ) : (
+                            <DetailBlock color="#db515f" icon={icon({name: 'x'})} title={`${entry.misses || 0} miss / ${entry.badCuts || 0} bad cut`} value={((entry.misses || 0) + (entry.badCuts || 0)) || 0} />
+                        )
+                    }
+
+                    <DetailBlock icon={icon({name: 'percent'})} title={`Accuracy: ${entry.accuracy}`} value={(parseFloat(entry.accuracy || 0.00)).toFixed(2)} />
+                    <DetailBlock icon={icon({name: 'gear'})} title={`Modifiers Used: ${entry.modifiers}`} value={entry.modifiers.trim().split(` `).join(`, `)} />
+                </div>
             </div>
-        );
+        )
     }
 }
