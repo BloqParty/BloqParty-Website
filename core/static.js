8f51d06e22eb5e17c744a6903f90eb724366bd06
Syl <hi@sylviiu.dev>
2023-09-04 00:30:25 -0500

    fix static vars

--- core/static.js
+++ core/static.js
@@ -3,15 +3,6 @@ const { execSync } = require('child_process');
 
 const pkg = require(`../package.json`)
 
-console.debug(`Getting static variables...`);
-
-let staticVars = {};
-
-try {
-    if(require(`./session`).dev || process.argv.includes(`build`)) throw new Error(``);
-    staticVars = JSON.parse(fs.readFileSync(`./static.json`, `utf8`))
-} catch(e) {};
-
 console.debug(`Generating variables...`);
 
 const date = new Date();
@@ -23,7 +14,7 @@ let repo = pkg.repository.url;
 if(repo.startsWith(`git+`)) repo = repo.slice(4);
 if(repo.endsWith(`.git`)) repo = repo.slice(0, -4);
 
-Object.assign(staticVars, {
+let staticVars = {
     version: {
         value: execSync(`git rev-parse --short HEAD`).toString().trim(),
         full: execSync(`git rev-parse HEAD`).toString().trim(),
@@ -33,6 +24,10 @@ Object.assign(staticVars, {
     repository: repo,
     buildDate: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} UTC${offset < 0 ? `+` : `-`}${Math.abs(offset)}`,
     authors: [`Nuggo`, `Speecil`, `Syl`]
-});
+};
 
 fs.writeFileSync(`./static.json`, JSON.stringify(staticVars, null, 4));
+
+console.debug(`Successfully generated variables!`);
+
+module.exports = staticVars;
