aebbbb2aeb56d169dd799fb4f808a0e961f9e69a
Syl <hi@sylviiu.dev>
2023-09-04 00:09:56 -0500

    verbose defaults false

--- core/config.js
+++ core/config.js
@@ -1,8 +1,6 @@
 const defaults = {
     port: 3000,
-    verbose: true,
-
-    defaultImage: `https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png`
+    verbose: false,
 };
 
 let config = {};
