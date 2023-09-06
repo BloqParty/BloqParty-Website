ab72a855c54638bc22454d146077087a019a47e5
Syl <hi@sylviiu.dev>
2023-09-06 09:13:14 -0500

    fix pull and restart error

--- src/endpoints/pull-and-restart.js
+++ src/endpoints/pull-and-restart.js
@@ -4,7 +4,7 @@ module.exports = {
     method: `get`,
     endpoint: `/pullAndRestart`,
     handle: async ({ app }, req, res) => {
-        if(req.headers[`Authorization`] == bpPrivateApi) {
+        if(req.headers[`Authorization`] === bpPrivateApi) {
             require(`../../core/update`).check().then(updates => {
                 if(updates) {
                     res.send(`okay fine you've twisted my arm`);
@@ -14,16 +14,5 @@ module.exports = {
                 }
             });
         } else res.sendStatus(401);
-
-        if(await pfs.existsSync(`./static/${req.params.path}`)) {
-            console.debug(`Sending static file: ${req.params.path}`);
-            res.sendFile(`${process.cwd()}/static/${req.params.path}`)
-        } else try {
-            console.debug(`Static file not found: ${req.params.path}`);
-            app.render404(req, res);
-        } catch(e) {
-            console.error(e);
-            res.status(500).send(`Internal Server Error`);
-        }
     }
 }
