2cfb2fc9ee7144a56bb29c63712c0f5800f5d136
Syl <hi@sylviiu.dev>
2023-09-07 15:05:06 -0500

    dynamic navbar buttons; basic user navbar mgmt

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,7 @@
 import React, { Component, useEffect, useState } from 'react';
+import { motion, circOut, AnimatePresence } from 'framer-motion';
+
+import easings from '../../util/easings';
 
 import User from './navbar/user';
 
@@ -9,25 +12,83 @@ const row = {
     justifyContent: `center`,
 }
 
-export default class Navbar extends Component {
-    height = 60;
+export const height = 60;
+
+export default function Navbar() {
+    const [ navbarEntries, setNavbarEntries ] = useState([]);
+
+    const elements = [
+        User
+    ].map((Element, i) => {
+        const [ theseEntries, setEntries ] = useState([]);
+
+        const [ state, setState ] = useState({
+            navbarShowing: []
+        });
     
-    render() {
+        const opts = {
+            navbar: {
+                entries: theseEntries,
+                get showing() {
+                    return state.navbarShowing.length;
+                },
+                get current() {
+                    return theseEntries;
+                },
+                set: (arr) => {
+                    arr = arr.reverse();
+                    setEntries(arr);
+                    if(state.navbarShowing.length) opts.navbar.show();
+                },
+                show: () => {
+                    const showing = theseEntries.map((o, i2) => {
+                        return (
+                            <div key={i2} style={{
+                                margin: `10px 0px`,
+                            }}>
+                                {o}
+                            </div>
+                        )
+                    });
+                    setState({ ...state, navbarShowing: showing });
+                    setNavbarEntries([...showing]);
+                },
+                hide: () => {
+                    setState({ ...state, navbarShowing: [] });
+                    setNavbarEntries([]);
+                }
+            }
+        };
+
         return (
-            <div id="navbar" style={{
+            <Element key={i} {...opts} />
+        )
+    })
+
+    return (
+        <div className="navbar" id="navbar" style={{
+            ...row,
+            flexDirection: `column`,
+            width: `100vw`,
+            padding: `0px 20px`,
+            boxSizing: `border-box`,
+            textAlign: `center`,
+            backgroundColor: `rgba(8, 8, 8, 0.5)`,
+            boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`,
+            backdropFilter: `blur(10px)`,
+            position: `fixed`,
+            top: `0px`,
+        }}>
+            <div className="navbar" style={{
                 ...row,
-                width: `100vw`,
-                height: `${this.height}px`,
-                padding: `0px 20px`,
-                boxSizing: `border-box`,
-                textAlign: `center`,
-                backgroundColor: `rgba(8, 8, 8, 0.5)`,
-                boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`,
-                backdropFilter: `blur(10px)`,
-                position: `fixed`,
-                top: `0px`,
+                width: `100%`,
+                minHeight: `${height}px`,
             }}>
-                <div style={{ ...row, justifyContent: `left` }}>
+                <div style={{
+                    ...row,
+                    justifyContent: `left`,
+                    height: `40px`
+                }}>
                     <h3 style={{
                         fontFamily: `Alata`,
                         fontWeight: `normal`,
@@ -35,10 +96,43 @@ export default class Navbar extends Component {
                     }}>Bedroom Party</h3>
                 </div>
 
-                <div style={{ ...row, flexGrow: 1, justifyContent: `right` }}>
-                    <User />
+                <div style={{
+                    ...row,
+                    flexGrow: 1,
+                    justifyContent: `right`,
+                    height: `40px`
+                }}>
+                    {elements}
                 </div>
             </div>
-        );
+
+            <div style={{
+                ...row,
+                width: `100%`,
+                justifyContent: `right`
+            }}>
+                <AnimatePresence style={{ ...row, width: `100%`, justifyContent: `right` }}>
+                    {
+                        navbarEntries.map((o, i) => (
+                            <motion.div
+                                transition={{ duration: 0.5, ease: easings.out.expo, staggerChildren: 0.03, staggerDirection: -1 }}
+                                initial={{ opacity: 0, marginTop: `-24px`, marginBottom: `-24px`, x: -200 }}
+                                animate={{ opacity: 1, marginTop: `0px`, marginBottom: `0px`, x: 0 }}
+                                exit={{ opacity: 0, marginTop: `-24px`, marginBottom: `-24px`, x: -20, color: `rgba(255,255,255,0)` }}
+                            >
+                                {navbarEntries}
+                            </motion.div>
+                        ))
+                    }
+                </AnimatePresence>
+            </div>
+        </div>
+    );
+};
+
+Navbar.height = height;
+
+/*export default class Navbar extends Component {
+    render() {
     }
-}
+}*/
--- src/components/navbar/user.jsx
+++ src/components/navbar/user.jsx
@@ -1,42 +1,106 @@
-import React, { Component } from 'react';
-import { instanceOf } from 'prop-types';
+import React, { Component, useState, useEffect, ReactDOM } from 'react';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { withCookies, Cookies } from 'react-cookie';
 
 import DetailBlock from '../detailblock';
+import Spinner from '../spinner';
+import Dropdown from '../dropdown';
 
-class User extends Component {
-    static propTypes = {
-        cookies: instanceOf(Cookies).isRequired,
-    };
+function User({ cookies, navbar }) {
+    const [ state, setState ] = useState({
+        loading: true
+    });
 
-    constructor(props) {
-        super(props);
+    console.log(`navbar`, navbar.current, navbar.showing);
 
-        const { cookies } = props;
-        this.state = {
-            token: cookies.get('token') || null,
+    useEffect(() => {
+        if(state.user) return;
 
-            name: cookies.get('name') || null,
-            id: cookies.get('id') || null,
-            avatarURL: cookies.get('avatarURL') || null,
-        };
+        const { id, key } = (cookies.get('auth') || {});
 
-        console.log(`state`, this.state)
-    }
+        console.log(`id`, id);
 
-    render() {
-        return (
-            <div style={{
-                display: `flex`,
-                flexDirection: `column`,
-                alignItems: `center`,
-                justifyContent: `center`,
-            }}>
-                <DetailBlock href="/login" value="Login, but you actually don't login and are left with the state of sadness" icon={icon({name: 'user'})} />
-            </div>
-        )
-    }
+        if(!id || !key) {
+            setState({
+                loading: false,
+                exists: false
+            });
+            navbar.set([
+                <DetailBlock href="/login" value="Login" icon={icon({name: 'user'})} /> 
+            ]);
+        } else {
+            fetch(`https://api.thebedroom.party/user/${id}`)
+                .then(res => res.json())
+                .then((user) => {
+                    setState({
+                        loading: false,
+                        exists: true,
+                        user: {
+                            name: user.username,
+                            id: user.game_id,
+                            avatarURL: user.avatar,
+                        }
+                    });
+                    navbar.set([
+                        <DetailBlock href="/login" value="Login" icon={icon({name: 'user'})} /> 
+                    ]);
+                })
+                .catch(e => {
+                    setState({
+                        loading: false,
+                        exists: false,
+                        error: `${e}`
+                    });
+                    navbar.set([
+                        <DetailBlock href="/login" value={`${e}`} color="#c24c44" icon={icon({name: 'exclamation-circle'})} />
+                    ]);
+                });
+        }
+    }, []);
+
+    return (
+        <a onClick={() => {
+            if(navbar.showing) {
+                console.log(`navbar is showing; hide`, navbar.showing, navbar.current);
+                navbar.hide();
+            } else {
+                console.log(`navbar is hidden; show`, navbar.showing, navbar.current);
+                navbar.show();
+            }
+        }}>
+            {
+                state.error ? 
+                    <DetailBlock href="/login" value={state.error} color="#c24c44" icon={icon({name: 'exclamation-circle'})} />
+                : state.loading ? 
+                    <Spinner />
+                : (
+                    !state.exists ? 
+                        <DetailBlock href="/login" value="Login" icon={icon({name: 'user'})} /> 
+                    : (
+                        <div style={{
+                            display: `flex`,
+                            flexDirection: `row`,
+                            alignItems: `center`,
+                            justifyContent: `center`,
+                        }}>
+                            <img src={state.user.avatarURL} style={{
+                                backgroundSize: `cover`,
+                                backgroundPosition: `center`,
+                                backgroundRepeat: `no-repeat`,
+                                backgroundColor: `rgba(0, 0, 0, 0.5)`,
+                                boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`,
+                                borderRadius: `100%`,
+                                marginRight: `6px`,
+                                width: `30px`,
+                                height: `30px`,
+                            }} />
+                            <DetailBlock value={state.user.name} icon={icon({name: 'user'})} />
+                        </div>
+                    )
+                )
+            }
+        </a>
+    )
 }
 
 export default withCookies(User);
