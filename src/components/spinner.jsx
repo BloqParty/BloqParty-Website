5c3ffa79f2050ad64f112457f45856c94bcda6ff
Syl <hi@sylviiu.dev>
2023-09-10 16:22:33 -0500

    allow changing size of spinner

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-export default function Spinner() {
-    return <FontAwesomeIcon icon={icon({name: 'circle-notch'})} spin style={{width: `20px`, height: `20px`}} />
+export default class Spinner extends Component {
+    render() {
+        return <FontAwesomeIcon icon={icon({name: 'circle-notch'})} spin style={{
+            width: this.props.size || `20px`,
+            height: this.props.size || `20px`,
+            ...(this.props.style || {})
+        }} />
+    }
 }
