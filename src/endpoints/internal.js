2be66c9a1a07f57daaf64d1e455786d87d8c6329
Syl <hi@sylviiu.dev>
2023-09-11 18:00:05 -0500

    create internal loginByKey endpoint for auth

--- src/endpoints/internal.js
+++ src/endpoints/internal.js
@@ -3,13 +3,35 @@ const { api } = require(`../../core/config`);
 
 module.exports = [
     {
+        name: `loginByKey`,
+        method: `get`,
+        endpoint: `/internal/ui/login`,
+        handle: ({ app }, req, res) => {
+            const { id, key } = req.universalCookies.get(`auth`);
+
+            console.debug(`Attempting login for ${id}`);
+
+            superagent.post(`https://dev.thebedroom.party/user/login`).set(`Authorization`, key).send({
+                id,
+                session: false
+            }).then(r => {
+                const user = JSON.parse(r.text);
+                console.debug(`Login successful:`, user);
+                res.send(user);
+            }).catch(e => {
+                console.error(`Error fetching user data:`, e);
+                res.send({ error: `Error fetching user data: ${e?.error || e}` });
+            });
+        },
+    },
+    {
         name: `uploadAvatar`,
         method: `post`,
         endpoint: `/internal/avatar/upload`,
         handle: ({ app }, req, res) => {
             const { id, key } = req.universalCookies.get(`auth`);
 
-            console.debug(`Uploading avatar for ${id}`, req.body);
+            console.debug(`Uploading avatar for ${id} with body length of ${req.body?.avatar.length}`);
 
             if(!req.body?.avatar) {
                 console.debug(`No avatar provided`);
