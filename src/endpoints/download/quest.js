3fc03e10eb42ae27a7352bac47e281b8d3fd88d4
Syl <hi@sylviiu.dev>
2023-09-24 12:25:56 -0500

    create quest mod downloading

---
+++ src/endpoints/download/quest.js
@@ -0,0 +1,40 @@
+const { Worker } = require(`worker_threads`);
+
+const getRelease = require(`../../../core/mod/getRelease`)
+
+module.exports = (req, res) => {
+    getRelease(`BPLBBETA-Quest`).then(release => {
+        if(!release) return res.status(500).send(`No release found`);
+
+        const worker = new Worker(`${__dirname}/util/quest.js`, {
+            workerData: {
+                user: req.user,
+                config: require(`../../../core/config`),
+                release
+            }
+        });
+
+        worker.on(`message`, data => {
+            if(data.zip) {
+                console.debug(`Sending zip`, data);
+                res.setHeader(`Content-Disposition`, `attachment; filename="BPLB-Quest ${data.version}.qmod"`);
+                res.setHeader(`Content-Type`, `application/zip`);
+                res.send(Buffer.from(data.zip));
+            } else if(data.error) {
+                res.status(500).send(data.error);
+            } else console.debug(`Unknown message from worker:`, data);
+        });
+
+        worker.on(`error`, e => {
+            console.error(`Error in worker:`, e);
+            res.status(500).send(e);
+        });
+
+        worker.on(`exit`, code => {
+            if(code !== 0) {
+                console.error(`Worker stopped with exit code ${code}`);
+                res.status(500).send(`Worker stopped with exit code ${code}`);
+            }
+        });
+    })
+}
