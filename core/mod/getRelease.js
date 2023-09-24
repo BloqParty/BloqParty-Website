c48ff99d81f3d6f415d5ba36132c700a126a9326
Syl <hi@sylviiu.dev>
2023-09-24 12:25:49 -0500

    getRelease for multiple repos support

--- core/mod/getRelease.js
+++ core/mod/getRelease.js
@@ -2,31 +2,33 @@ const superagent = require(`superagent`);
 
 const { api } = require(`../config`)
 
-let latest = null, promise;
+const versions = {};
 
-module.exports = () => {
-    if(promise) {
-        return promise;
-    } else if(latest && ((Date.now() + 1.8e+6) < latest.fetched)) { // 30 mins
-        return Promise.resolve(latest);
+module.exports = (repo) => {
+    if(!versions[repo]) versions[repo] = {};
+
+    if(versions[repo].promise) {
+        return versions[repo].promise;
+    } else if(versions[repo].latest && ((Date.now() + 1.8e+6) < versions[repo].latest.fetched)) { // 30 mins
+        return Promise.resolve(versions[repo].latest);
     } else {
-        promise = new Promise(async res => {
+        versions[repo].promise = new Promise(async res => {
             console.log(`Fetching latest release... (token: "${api.githubAccessToken}")`);
 
-            superagent.get(`https://api.github.com/repos/BedroomParty/BPLBBETA/releases/latest`)
+            superagent.get(`https://api.github.com/repos/BedroomParty/${repo}/releases/latest`)
                 .set(`Authorization`, api.githubAccessToken)
                 .set(`User-Agent`, `node`)
                 .then(({body}) => {
-                    latest = Object.assign(body, { fetched: Date.now() });
-                    promise = null;
-                    res(latest);
+                    versions[repo].latest = Object.assign(body, { fetched: Date.now() });
+                    versions[repo].promise = null;
+                    res(versions[repo].latest);
                 }).catch(e => {
                     console.error(`Failed to fetch latest release:`, e);
-                    promise = null;
+                    versions[repo].promise = null;
                     res(null);
                 });
         });
 
-        return promise;
+        return versions[repo].promise;
     }
 }
