4141708d486d249a8af93966419d632d56cca62a
Syl <hi@sylviiu.dev>
2023-09-29 23:35:55 -0500

    fixed bug that should've NEVER EXISTED??

--- src/styles/navbar.js
+++ src/styles/navbar.js
@@ -1,41 +1,49 @@
+let navlistenerset = false;
+
 if(typeof window != `undefined`) {
-    console.log(`Running navbar.js...`);
+    if(!navlistenerset) {
+        console.log(`Running navbar.js...`);
+        
+        let elm = document.querySelector(`.navbar-root`);
+        let fg = document.querySelector(`.fg`);
     
-    const elm = document.querySelector(`.navbar-root`);
-    const fg = document.querySelector(`.fg`);
-
-    let threshold = 10;
-    let lastScroll = 0;
-
-    const scrollFunc = () => {
-        if(window.innerWidth <= 600) {
-            const difference = lastScroll - fg.scrollTop;
-
-            if(Math.abs(difference) > threshold) {    
-                if(difference < 0 && !elm.classList.contains(`navbar-solid`)) {
-                    console.log(`Adding navbar-solid...`);
-                    elm.classList.add(`navbar-solid`);
-                    fg.classList.add(`fg-scrolled`);
-                } else if(difference > 0 && elm.classList.contains(`navbar-solid`)) {
-                    console.log(`Removing navbar-solid...`);
-                    elm.classList.remove(`navbar-solid`);
-                    fg.classList.remove(`fg-scrolled`);
+        if(elm && fg) {
+            let threshold = 10;
+            let lastScroll = 0;
+        
+            const scrollFunc = () => {        
+                if(window.innerWidth <= 600) {
+                    const difference = lastScroll - fg.scrollTop;
+        
+                    if(Math.abs(difference) > threshold) {    
+                        if(difference < 0 && !elm.classList.contains(`navbar-solid`)) {
+                            console.log(`Adding navbar-solid...`);
+                            elm.classList.add(`navbar-solid`);
+                            fg.classList.add(`fg-scrolled`);
+                        } else if(difference > 0 && elm.classList.contains(`navbar-solid`)) {
+                            console.log(`Removing navbar-solid...`);
+                            elm.classList.remove(`navbar-solid`);
+                            fg.classList.remove(`fg-scrolled`);
+                        }
+            
+                        lastScroll = fg.scrollTop;
+                    }
+                } else {
+                    if(fg.scrollTop > threshold && !elm.classList.contains(`navbar-solid`)) {
+                        console.log(`Adding navbar-solid...`);
+                        elm.classList.add(`navbar-solid`);
+                        fg.classList.add(`fg-scrolled`);
+                    } else if(fg.scrollTop <= threshold && elm.classList.contains(`navbar-solid`)) {
+                        console.log(`Removing navbar-solid...`);
+                        elm.classList.remove(`navbar-solid`);
+                        fg.classList.remove(`fg-scrolled`);
+                    }
                 }
-    
-                lastScroll = fg.scrollTop;
-            }
-        } else {
-            if(fg.scrollTop > threshold && !elm.classList.contains(`navbar-solid`)) {
-                console.log(`Adding navbar-solid...`);
-                elm.classList.add(`navbar-solid`);
-                fg.classList.add(`fg-scrolled`);
-            } else if(fg.scrollTop <= threshold && elm.classList.contains(`navbar-solid`)) {
-                console.log(`Removing navbar-solid...`);
-                elm.classList.remove(`navbar-solid`);
-                fg.classList.remove(`fg-scrolled`);
-            }
-        }
-    };
-
-    fg.addEventListener(`scroll`, scrollFunc);
+            };
+        
+            fg.addEventListener(`scroll`, scrollFunc);
+        
+            navlistenerset = true;
+        } else console.log(`Error: navbar.js: scrollFunc: elm or fg is null.`);
+    }
 } else console.log(`Skipping navbar.js...`);
