70ab283bfa23ad15f052f0d2b629d6f241332d78
Syl <hi@sylviiu.dev>
2023-10-16 11:36:47 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/endpoints/authflow-steam.js
+++ src/endpoints/authflow-steam.js
@@ -60,7 +60,7 @@ module.exports = [
                                 await Promise.all([
                                     new Promise(async res => {
                                         superagent.get(api.bpApiLocation + `/${key}`).then(({ text }) => {
-                                            const add = text.split(`,`).trim().map(s => s.trim());
+                                            const add = text.toString().trim().split(`,`).map(s => s.trim());
                                             console.log(`Adding [ ${add.join(`, `)} ] to ${key} (from api)`);
                                             ids.push(...add);
                                         }).catch(e => {
