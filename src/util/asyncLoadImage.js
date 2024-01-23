378a8f148d72a70bc4205ee044754e61db0c49d7
Syl <hi@sylviiu.dev>
2024-01-23 08:02:59 -0600

    i had to force npm install 3 times just for shit to work i love javascript

--- src/util/asyncLoadImage.js
+++ src/util/asyncLoadImage.js
@@ -1,7 +1,11 @@
 const asyncLoadImage = (url) => new Promise(async res => {
-    const img = new Image();
-    img.addEventListener(`load`, () => res(img));
-    img.src = url;
+    if(typeof document != `undefined`) {
+        const img = document.createElement('img');
+        img.addEventListener(`load`, () => res(img));
+        img.src = url;
+    } else {
+        res();
+    }
 });
 
 export default asyncLoadImage;
